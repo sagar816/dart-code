e1a64e9436976b350aba784c6458f21500ff1b69
sagart <sagart@gmail.com>
2024-07-11 15:28:38 +0530

    july_11_03

---
+++ lib/01_hello.dart
@@ -0,0 +1,24 @@
+void main() {
+  print("Hello World! Dart Code");
+}
+
+
+// this main will not run directly > run without debugging
+// you need to make one folder "lib" (library) and then move the main file in it to run
+//this is because VS code run flutter project
+//can run and check the above or selse i did the follwing setting suggested by vs code
+// {
+//     // Use IntelliSense to learn about possible attributes.
+//     // Hover to view descriptions of existing attributes.
+//     // For more information, visit: https://go.microsoft.com/fwlink/?linkid=830387
+//     "version": "0.2.0",
+//     "configurations": [
+        
+//         {
+//             "name": "Dart & Flutter",
+//             "request": "launch",
+//             "type": "dart",
+//             "program": "lib/hello.dart", >>> include path and file name to be executed
+//         }
+//     ]
+// }
---
+++ lib/02_main.dart
@@ -0,0 +1,29 @@
+void main() {
+  print("Dart Code"); //semicolon means line end
+  print(
+      "Sagar Tambatkar"); //will print in second line beacuse above semicolon is used.
+  print("");
+  // **************************************
+
+  print("Family Members");
+  print("1 . Father");
+  print("2 . Mother");
+  print("3 . GrandFather");
+  print("4 . GrandMother");
+  print("5 . Children");
+
+  print("");
+  // **************************************
+
+  print("Create a Square Shape");
+  print("");
+  print("******");
+  print("*    *");
+  print("*    *");
+  print("******");
+
+  print("");
+  // **************************************
+
+  print("Assignments 01");
+}
